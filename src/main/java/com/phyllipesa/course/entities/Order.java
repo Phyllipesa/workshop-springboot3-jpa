8842a2f55f7c777310952bdcb995bcc82c0c6095
Phyllipe <phylipe.sjc@hotmail.com>
2023-11-09 16:25:38 -0300

    OrderStatus enum

--- src/main/java/com/phyllipesa/course/entities/Order.java
+++ src/main/java/com/phyllipesa/course/entities/Order.java
@@ -1,6 +1,7 @@
 package com.phyllipesa.course.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.phyllipesa.course.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -17,6 +18,8 @@ public class Order implements Serializable {
   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
   private Instant moment;
 
+  private Integer orderStatus;
+
   @ManyToOne
   @JoinColumn(name = "client_id")
   private User client;
@@ -24,9 +27,10 @@ public class Order implements Serializable {
   public Order() {
   }
 
-  public Order(Long id, Instant moment, User client) {
+  public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
     this.id = id;
     this.moment = moment;
+    setOrderStatus(orderStatus);
     this.client = client;
   }
 
@@ -46,6 +50,16 @@ public class Order implements Serializable {
     this.moment = moment;
   }
 
+  public OrderStatus getOrderStatus() {
+    return OrderStatus.valueOf(orderStatus);
+  }
+
+  public void setOrderStatus(OrderStatus orderStatus) {
+    if (orderStatus != null) {
+      this.orderStatus = orderStatus.getCode();
+    }
+  }
+
   public User getClient() {
     return client;
   }
@@ -53,6 +67,7 @@ public class Order implements Serializable {
   public void setClient(User client) {
     this.client = client;
   }
+
 
   @Override
   public boolean equals(Object o) {
