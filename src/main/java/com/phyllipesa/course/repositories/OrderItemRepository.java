533923b2bef1ce2fb6e06c784c574c2734481f6f
Phyllipe <phylipe.sjc@hotmail.com>
2023-11-10 15:29:57 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/phyllipesa/course/repositories/OrderItemRepository.java
+++ src/main/java/com/phyllipesa/course/repositories/OrderItemRepository.java
@@ -1,6 +1,5 @@
 package com.phyllipesa.course.repositories;
 
-import com.phyllipesa.course.entities.Category;
 import com.phyllipesa.course.entities.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
